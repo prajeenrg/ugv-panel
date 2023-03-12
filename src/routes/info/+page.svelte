f9fbb2b86fada8aead4b0a48cebe5c7f9d67172b
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-03-12 17:55:46 +0530

    fixed issue with mqtt not bundling in prod build

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,57 +1,60 @@
 <script lang="ts">
-	import helper from "../mqtthelper";
-    import { onMount, onDestroy } from "svelte";
+	import { client, data, setupClient, disconnectClient, Constants } from '../mqtthelper';
+	import { onMount, onDestroy } from 'svelte';
 
-    onMount(helper.setupClient);
-    
-    onDestroy(() => {
-        helper.client.end();
-        console.info("Disconnected MQTT Client from Broker");
-    });
+	onMount(setupClient);
 
-    const client = helper.client;
+	onDestroy(disconnectClient);
 
-    $: data = helper.data;
-    $: isConnected = client.connected;
+	let topic = '';
+	let msg = '';
 
-    let topic = "";
-    let msg = "";
+	let handleClick = async () => {
+		await client.publish(Constants.TOPIC_INFO_GPS, `${Date.now()}`);
+		console.info('Message sent');
+	};
 
-    let handleClick = () => helper.publish(topic, msg);
+    $: status = client.connectionState;
+    $: subs = client.subscriptions
 </script>
 
-{#if isConnected}
-    <p>Waiting for the MQTT Client to establish connection...</p>
-{:else}
-    <p>LiDAR Data</p>
-    <ul>
-        <li>Front: {data.lidar.front} mm</li>
-        <li>Back: {data.lidar.back} mm</li>
-        <li>Left: {data.lidar.left} mm</li>
-        <li>Right: {data.lidar.right} mm</li>
-    </ul>
+<!-- {#if status !== 'connected' }
+	<p>Waiting for the MQTT Client to establish connection...</p>
+{:else} -->
+	<ol>
+		{#each subs as sub}
+			<li>{sub.topicFilter}</li>
+		{/each}
+	</ol>
+	<p>LiDAR Data</p>
+	<ul>
+		<li>Front: {$data.lidar.front} mm</li>
+		<li>Back: {$data.lidar.back} mm</li>
+		<li>Left: {$data.lidar.left} mm</li>
+		<li>Right: {$data.lidar.right} mm</li>
+	</ul>
 
-    <p>GPS Data</p>
-    <ul>
-        <li>Coordinates: {data.gps.coordinates}</li>
-    </ul>
+	<p>GPS Data</p>
+	<ul>
+		<li>Coordinates: {$data.gps.coordinates}</li>
+	</ul>
 
-    <p>Motor Data</p>
-    <ul>
-        <li>Left Motor Speed: {data.motor.leftSpeed} RPM</li>
-        <li>Right Motor Speed: {data.motor.rightSpeed} RPM</li>
-    </ul>
+	<p>Motor Data</p>
+	<ul>
+		<li>Left Motor Speed: {$data.motor.leftSpeed} RPM</li>
+		<li>Right Motor Speed: {$data.motor.rightSpeed} RPM</li>
+	</ul>
 
-    <p>Gyro Data</p>
-    <ul>
-        <li>X-Axis: {data.gyro.xAxis}</li>
-        <li>Y-Axis: {data.gyro.yAxis}</li>
-        <li>Z-Axis: {data.gyro.zAxis}</li>
-        <li>Acceleration: {data.gyro.accel}</li>
-    </ul>
+	<p>Gyro Data</p>
+	<ul>
+		<li>X-Axis: {$data.gyro.xAxis}</li>
+		<li>Y-Axis: {$data.gyro.yAxis}</li>
+		<li>Z-Axis: {$data.gyro.zAxis}</li>
+		<li>Acceleration: {$data.gyro.accel}</li>
+	</ul>
 
-    <p>Send your data here</p>
-    <input bind:value={topic}>
-    <input bind:value={msg}>
-    <button on:click={handleClick}>Send</button>
-{/if}
+	<p>Send your data here</p>
+	<input bind:value={topic} />
+	<input bind:value={msg} />
+	<button on:click={handleClick}>Send</button>
+<!-- {/if} -->
--- src/routes/mqtthelper.ts
+++ src/routes/mqtthelper.ts
@@ -1,34 +1,28 @@
-import { connect, MqttClient } from "mqtt";
+import { Client } from "@jdiamond/mqtt-browser";
+import { writable } from "svelte/store";
 
 // constant variables
 const BROKER_URL = "ws://broker.emqx.io:8083/mqtt";
 const TOPIC_PREFIX = "c83929b2-c031-11ed-afa1-0242ac120002";
 
-const TOPIC_INFO_LIDAR_FRONT = "info/lidar/front";
-const TOPIC_INFO_LIDAR_BACK = "info/lidar/back";
-const TOPIC_INFO_LIDAR_LEFT = "info/lidar/left";
-const TOPIC_INFO_LIDAR_RIGHT = "info/lidar/right";
+const TOPIC_INFO_LIDAR_FRONT = `${TOPIC_PREFIX}/info/lidar/front`;
+const TOPIC_INFO_LIDAR_BACK = `${TOPIC_PREFIX}/info/lidar/back`;
+const TOPIC_INFO_LIDAR_LEFT = `${TOPIC_PREFIX}/info/lidar/left`;
+const TOPIC_INFO_LIDAR_RIGHT = `${TOPIC_PREFIX}/info/lidar/right`;
 
-const TOPIC_INFO_GPS = "info/gps";
+const TOPIC_INFO_GPS = `${TOPIC_PREFIX}/info/gps`;
 
-const TOPIC_INFO_MOTOR_LEFT = "info/motor/left";
-const TOPIC_INFO_MOTOR_RIGHT = "info/motor/right";
+const TOPIC_INFO_MOTOR_LEFT = `${TOPIC_PREFIX}/info/motor/left`;
+const TOPIC_INFO_MOTOR_RIGHT = `${TOPIC_PREFIX}info/motor/right`;
 
-const TOPIC_INFO_GYRO_X = "info/gyro/x";
-const TOPIC_INFO_GYRO_Y = "info/gyro/y";
-const TOPIC_INFO_GYRO_Z = "info/gyro/z";
-const TOPIC_INFO_GYRO_ACCEL = "info/gyro/accel";
+const TOPIC_INFO_GYRO_X = `${TOPIC_PREFIX}/info/gyro/x`;
+const TOPIC_INFO_GYRO_Y = `${TOPIC_PREFIX}/info/gyro/y`;
+const TOPIC_INFO_GYRO_Z = `${TOPIC_PREFIX}/info/gyro/z`;
+const TOPIC_INFO_GYRO_ACCEL = `${TOPIC_PREFIX}/info/gyro/accel`;
 
-let client: MqttClient = connect(BROKER_URL);
+export const client = new Client({ url: BROKER_URL });
 
 // Data interface
-interface UgvData {
-    lidar: LidarData
-    gps: GpsData
-    motor: MotorData
-    gyro: GyroData
-}
-
 interface LidarData {
     front: number
     back: number
@@ -52,49 +46,62 @@ interface GyroData {
     accel: number
 }
 
-const setupClient = () => {
-    client = connect(BROKER_URL);
+export const setupClient = async () => {
+    await client.connect();
+    console.info("Connected to MQTT Broker successfully");
     // subscriptions
-    client.on("connect", () => {
-        console.info(`Connected to MQTT Broker at ${BROKER_URL} successfully`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_LIDAR_FRONT}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_LIDAR_BACK}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_LIDAR_LEFT}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_LIDAR_RIGHT}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_GPS}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_MOTOR_LEFT}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_MOTOR_RIGHT}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_GYRO_X}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_GYRO_Y}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_GYRO_Z}`);
-        client.subscribe(`${TOPIC_PREFIX}/${TOPIC_INFO_GYRO_ACCEL}`);
-        console.info("Subscribed to topics successfully");
-    });
-    // message handler
-    client.on("message", (topic, message) => {
-        console.info(topic, String.fromCharCode(...message));
-        if (topic.includes(TOPIC_INFO_GPS)) {
-            console.log("matched");
-            data.gps.coordinates = parseInt(message.toString());
+    console.info(`Connected to MQTT Broker at ${BROKER_URL} successfully`);
+    client.subscribe(TOPIC_INFO_LIDAR_FRONT);
+    client.subscribe(TOPIC_INFO_LIDAR_BACK);
+    client.subscribe(TOPIC_INFO_LIDAR_LEFT);
+    client.subscribe(TOPIC_INFO_LIDAR_RIGHT);
+    client.subscribe(TOPIC_INFO_GPS);
+    client.subscribe(TOPIC_INFO_MOTOR_LEFT);
+    client.subscribe(TOPIC_INFO_MOTOR_RIGHT);
+    client.subscribe(TOPIC_INFO_GYRO_X);
+    client.subscribe(TOPIC_INFO_GYRO_Y);
+    client.subscribe(TOPIC_INFO_GYRO_Z);
+    client.subscribe(TOPIC_INFO_GYRO_ACCEL);
+    console.info("Subscribed to topics successfully");
+    // handle messages
+    client.on("message", (topic: string, message: any) => {
+        console.log(`Received message on topic ${topic}`);
+        let msg = String.fromCharCode(...message);
+        switch (topic) {
+            case TOPIC_INFO_GPS:
+                update(curr => {
+                    curr.gps.coordinates = parseInt(msg);
+                    return curr;
+                });
+                break;
+            default:
+                console.info("Unknown topic...");
         }
-    });
+    })
 };
 
-let data: UgvData = {
-    lidar: {
+export const disconnectClient = async () => {
+    if (client.connectionState === "connected") {
+        await client.disconnect();
+    }
+    console.info("Disconnected from MQTT Broker");
+};
+
+let defaultData = {
+    lidar: <LidarData> {
         front: 0,
         back: 0,
         left: 0,
         right: 0
     },
-    gps: {
+    gps: <GpsData> {
         coordinates: 0
     },
-    motor: {
+    motor: <MotorData> {
         leftSpeed: 0,
         rightSpeed: 0
     },
-    gyro: {
+    gyro: <GyroData> {
         xAxis: 0,
         yAxis: 0,
         zAxis: 0,
@@ -102,6 +109,16 @@ let data: UgvData = {
     }
 };
 
-const publish = (topic: string, message: any) => client.publish(`${TOPIC_PREFIX}/${topic}`, message);
+const { set, update, subscribe } = writable(defaultData);
 
-export default { client, data, setupClient, publish };
+export const data = { subscribe };
+
+export const Constants = {
+    BROKER_URL,
+    TOPIC_PREFIX,
+    TOPIC_INFO_GPS,
+    TOPIC_INFO_GYRO_ACCEL,
+    TOPIC_INFO_GYRO_X,
+    TOPIC_INFO_GYRO_Y,
+    TOPIC_INFO_GYRO_Z
+};
