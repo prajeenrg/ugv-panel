830d46e76c356a1cded33f3d788f9972fa6890cf
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-03-12 19:29:56 +0530

    topic to receive json as message to simply the process

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -10,51 +10,56 @@
 	let msg = '';
 
 	let handleClick = async () => {
-		await client.publish(Constants.TOPIC_INFO_GPS, `${Date.now()}`);
+		if (!topic || !msg) return;
+		await client.publish(`${Constants.TOPIC_PREFIX}/${topic}`, msg);
 		console.info('Message sent');
 	};
-
-    $: status = client.connectionState;
-    $: subs = client.subscriptions
 </script>
 
 <!-- {#if status !== 'connected' }
 	<p>Waiting for the MQTT Client to establish connection...</p>
 {:else} -->
-	<ol>
-		{#each subs as sub}
-			<li>{sub.topicFilter}</li>
-		{/each}
-	</ol>
-	<p>LiDAR Data</p>
-	<ul>
-		<li>Front: {$data.lidar.front} mm</li>
-		<li>Back: {$data.lidar.back} mm</li>
-		<li>Left: {$data.lidar.left} mm</li>
-		<li>Right: {$data.lidar.right} mm</li>
-	</ul>
+<p>LiDAR Data</p>
+<ul>
+	<li>Front: {$data.lidar.front} mm</li>
+	<li>Back: {$data.lidar.back} mm</li>
+	<li>Left: {$data.lidar.left} mm</li>
+	<li>Right: {$data.lidar.right} mm</li>
+</ul>
 
-	<p>GPS Data</p>
-	<ul>
-		<li>Coordinates: {$data.gps.coordinates}</li>
-	</ul>
+<p>GPS Data</p>
+<ul>
+	<li>Coordinates: {$data.gps.coordinates}</li>
+</ul>
 
-	<p>Motor Data</p>
-	<ul>
-		<li>Left Motor Speed: {$data.motor.leftSpeed} RPM</li>
-		<li>Right Motor Speed: {$data.motor.rightSpeed} RPM</li>
-	</ul>
+<p>Motor Data</p>
+<ul>
+	<li>Left Motor Speed: {$data.motor.leftSpeed} RPM</li>
+	<li>Right Motor Speed: {$data.motor.rightSpeed} RPM</li>
+</ul>
 
-	<p>Gyro Data</p>
-	<ul>
-		<li>X-Axis: {$data.gyro.xAxis}</li>
-		<li>Y-Axis: {$data.gyro.yAxis}</li>
-		<li>Z-Axis: {$data.gyro.zAxis}</li>
-		<li>Acceleration: {$data.gyro.accel}</li>
-	</ul>
+<p>Gyro Data</p>
+<ul>
+	<li>X-Axis: {$data.gyro.xAxis}</li>
+	<li>Y-Axis: {$data.gyro.yAxis}</li>
+	<li>Z-Axis: {$data.gyro.zAxis}</li>
+	<li>Acceleration: {$data.gyro.accel}</li>
+</ul>
 
-	<p>Send your data here</p>
-	<input bind:value={topic} />
-	<input bind:value={msg} />
+<p>Send your data here</p>
+<div>
+	<input placeholder="Enter topic name" bind:value={topic} />
+	<textarea placeholder="Enter message" rows="10" bind:value={msg} />
 	<button on:click={handleClick}>Send</button>
+</div>
+
 <!-- {/if} -->
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		row-gap: 10px;
+		max-width: 40vw;
+	}
+</style>
--- src/routes/mqtthelper.ts
+++ src/routes/mqtthelper.ts
@@ -1,112 +1,122 @@
-import { Client } from "@jdiamond/mqtt-browser";
-import { writable } from "svelte/store";
+import { Client } from '@jdiamond/mqtt-browser';
+import { writable } from 'svelte/store';
 
 // constant variables
-const BROKER_URL = "wss://test.mosquitto.org:8081";
-const TOPIC_PREFIX = "c83929b2-c031-11ed-afa1-0242ac120002";
+const BROKER_URL = 'wss://test.mosquitto.org:8081';
+const TOPIC_PREFIX = 'c83929b2-c031-11ed-afa1-0242ac120002';
 
-const TOPIC_INFO_LIDAR_FRONT = `${TOPIC_PREFIX}/info/lidar/front`;
-const TOPIC_INFO_LIDAR_BACK = `${TOPIC_PREFIX}/info/lidar/back`;
-const TOPIC_INFO_LIDAR_LEFT = `${TOPIC_PREFIX}/info/lidar/left`;
-const TOPIC_INFO_LIDAR_RIGHT = `${TOPIC_PREFIX}/info/lidar/right`;
+const TOPIC_INFO_LIDAR = `${TOPIC_PREFIX}/info/lidar`;
 
 const TOPIC_INFO_GPS = `${TOPIC_PREFIX}/info/gps`;
 
-const TOPIC_INFO_MOTOR_LEFT = `${TOPIC_PREFIX}/info/motor/left`;
-const TOPIC_INFO_MOTOR_RIGHT = `${TOPIC_PREFIX}info/motor/right`;
+const TOPIC_INFO_MOTOR = `${TOPIC_PREFIX}/info/motor`;
 
-const TOPIC_INFO_GYRO_X = `${TOPIC_PREFIX}/info/gyro/x`;
-const TOPIC_INFO_GYRO_Y = `${TOPIC_PREFIX}/info/gyro/y`;
-const TOPIC_INFO_GYRO_Z = `${TOPIC_PREFIX}/info/gyro/z`;
-const TOPIC_INFO_GYRO_ACCEL = `${TOPIC_PREFIX}/info/gyro/accel`;
+const TOPIC_INFO_GYRO = `${TOPIC_PREFIX}/info/gyro`;
 
 export const client = new Client({ url: BROKER_URL });
 
 // Data interface
 interface LidarData {
-    front: number
-    back: number
-    left: number
-    right: number
+	front: number;
+	back: number;
+	left: number;
+	right: number;
 }
 
 interface GpsData {
-    coordinates: number
+	coordinates: number;
 }
 
 interface MotorData {
-    leftSpeed: number
-    rightSpeed: number
+	leftSpeed: number;
+	rightSpeed: number;
 }
 
 interface GyroData {
-    xAxis: number
-    yAxis: number
-    zAxis: number
-    accel: number
+	xAxis: number;
+	yAxis: number;
+	zAxis: number;
+	accel: number;
 }
 
 export const setupClient = async () => {
-    await client.connect();
-    console.info("Connected to MQTT Broker successfully");
-    // subscriptions
-    console.info(`Connected to MQTT Broker at ${BROKER_URL} successfully`);
-    client.subscribe(TOPIC_INFO_LIDAR_FRONT);
-    client.subscribe(TOPIC_INFO_LIDAR_BACK);
-    client.subscribe(TOPIC_INFO_LIDAR_LEFT);
-    client.subscribe(TOPIC_INFO_LIDAR_RIGHT);
-    client.subscribe(TOPIC_INFO_GPS);
-    client.subscribe(TOPIC_INFO_MOTOR_LEFT);
-    client.subscribe(TOPIC_INFO_MOTOR_RIGHT);
-    client.subscribe(TOPIC_INFO_GYRO_X);
-    client.subscribe(TOPIC_INFO_GYRO_Y);
-    client.subscribe(TOPIC_INFO_GYRO_Z);
-    client.subscribe(TOPIC_INFO_GYRO_ACCEL);
-    console.info("Subscribed to topics successfully");
-    // handle messages
-    client.on("message", (topic: string, message: any) => {
-        console.log(`Received message on topic ${topic}`);
-        let msg = String.fromCharCode(...message);
-        switch (topic) {
-            case TOPIC_INFO_GPS:
-                update(curr => {
-                    curr.gps.coordinates = parseInt(msg);
-                    return curr;
-                });
-                break;
-            default:
-                console.info("Unknown topic...");
-        }
-    })
+	await client.connect();
+	console.info('Connected to MQTT Broker successfully');
+	// subscriptions
+	console.info(`Connected to MQTT Broker at ${BROKER_URL} successfully`);
+	client.subscribe(TOPIC_INFO_LIDAR);
+	client.subscribe(TOPIC_INFO_GPS);
+	client.subscribe(TOPIC_INFO_MOTOR);
+	client.subscribe(TOPIC_INFO_GYRO);
+	console.info('Subscribed to topics successfully');
+	// handle messages
+	client.on('message', handleMessages);
+};
+
+const handleMessages = (topic: string, message: any) => {
+	let msg = String.fromCharCode(...message);
+	console.log(`Received ${msg} on topic ${topic}`);
+	try {
+		switch (topic) {
+			case TOPIC_INFO_GPS:
+				update((curr) => {
+					curr.gps = JSON.parse(msg);
+					return curr;
+				});
+				break;
+			case TOPIC_INFO_GYRO:
+				update((curr) => {
+					curr.gyro = JSON.parse(msg);
+					return curr;
+				});
+				break;
+			case TOPIC_INFO_LIDAR:
+				update((curr) => {
+					curr.lidar = JSON.parse(msg);
+					return curr;
+				});
+				break;
+			case TOPIC_INFO_MOTOR:
+				update((curr) => {
+					curr.motor = JSON.parse(msg);
+					return curr;
+				});
+				break;
+			default:
+				console.info('Unknown topic...');
+		}
+	} catch (e) {
+		console.error(`Error while parsing JSON for topic ${topic}`);
+	}
 };
 
 export const disconnectClient = async () => {
-    if (client.connectionState === "connected") {
-        await client.disconnect();
-    }
-    console.info("Disconnected from MQTT Broker");
+	if (client.connectionState === 'connected') {
+		await client.disconnect();
+	}
+	console.info('Disconnected from MQTT Broker');
 };
 
 let defaultData = {
-    lidar: <LidarData> {
-        front: 0,
-        back: 0,
-        left: 0,
-        right: 0
-    },
-    gps: <GpsData> {
-        coordinates: 0
-    },
-    motor: <MotorData> {
-        leftSpeed: 0,
-        rightSpeed: 0
-    },
-    gyro: <GyroData> {
-        xAxis: 0,
-        yAxis: 0,
-        zAxis: 0,
-        accel: 0
-    }
+	lidar: <LidarData>{
+		front: 0,
+		back: 0,
+		left: 0,
+		right: 0
+	},
+	gps: <GpsData>{
+		coordinates: 0
+	},
+	motor: <MotorData>{
+		leftSpeed: 0,
+		rightSpeed: 0
+	},
+	gyro: <GyroData>{
+		xAxis: 0,
+		yAxis: 0,
+		zAxis: 0,
+		accel: 0
+	}
 };
 
 const { set, update, subscribe } = writable(defaultData);
@@ -114,11 +124,10 @@ const { set, update, subscribe } = writable(defaultData);
 export const data = { subscribe };
 
 export const Constants = {
-    BROKER_URL,
-    TOPIC_PREFIX,
-    TOPIC_INFO_GPS,
-    TOPIC_INFO_GYRO_ACCEL,
-    TOPIC_INFO_GYRO_X,
-    TOPIC_INFO_GYRO_Y,
-    TOPIC_INFO_GYRO_Z
+	BROKER_URL,
+	TOPIC_PREFIX,
+	TOPIC_INFO_GPS,
+	TOPIC_INFO_GYRO,
+	TOPIC_INFO_LIDAR,
+	TOPIC_INFO_MOTOR
 };
