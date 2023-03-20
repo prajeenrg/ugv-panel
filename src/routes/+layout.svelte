9689d95078f8e4e2f0e2f3cd3b6ddd277f48f431
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-03-20 23:07:32 +0530

    ui modifications for navbar and home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	export const appTitle = 'MIT UGV Control Panel';
+	export const appTitle = 'RhoVar';
 
 	export const navlinks = [
 		{
@@ -16,35 +16,42 @@
 	];
 </script>
 
-<nav>
-	<p>{appTitle}</p>
-	<ul>
-		{#each navlinks as navlink (navlink.id)}
-			<li>
-				<a
-					href={navlink.href}
-					style:color={$page.url.pathname == navlink.href
-						? 'var(--nav-item-active)'
-						: 'var(--nav-item-inactive)'}>{navlink.name}</a
-				>
-			</li>
-		{/each}
-	</ul>
-</nav>
+<header>
+	<div class="color-strip" />
+	<nav>
+		<div class="title">
+			<img src="/logo.png" class="logo" alt="rover" />
+			<p>{appTitle}</p>
+		</div>
+		<ul>
+			{#each navlinks as navlink (navlink.id)}
+				<li>
+					<a
+						href={navlink.href}
+						style:color={$page.url.pathname == navlink.href
+							? 'var(--nav-item-active)'
+							: 'var(--nav-item-inactive)'}>{navlink.name}</a
+					>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
 
 <slot />
 
 <style>
 	:root {
-		--bg-main-color: #430ee1;
-		--nav-item-active: hsla(0, 0%, 100%, 0.87);
-		--nav-item-inactive: hsla(0, 0%, 100%, 0.6);
-		--text-primary-color: hsla(0, 0%, 100%, 1);
+		--bg-main-color: hsl(0, 0%, 100%);
+		--nav-item-active: hsla(0, 0%, 0%, 0.87);
+		--nav-item-inactive: hsla(0, 0%, 0%, 0.4);
+		--nav-item-disabled: hsla(0, 0%, 0%, 0.15);
+		--text-primary-color: hsla(0, 0%, 0%, 1);
 	}
 
 	:global(*) {
 		margin: 0;
-		font-family: 'Space Grotesk', sans-serif;
+		font-family: 'Poppins', sans-serif;
 		box-sizing: border-box;
 	}
 
@@ -52,22 +59,47 @@
 		margin-top: 84px;
 	}
 
-	nav {
+	header {
 		position: fixed;
 		top: 0;
+		left: 0;
 		width: 100%;
+		z-index: 5;
+	}
+
+	.color-strip {
+		height: 16px;
+		z-index: 10;
+		background: linear-gradient(90deg, hsl(46, 95%, 56%), hsl(350, 97%, 65%));
+	}
+
+	.title {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		height: 100%;
+	}
+
+	.logo {
+		height: 56px;
+		padding: 4px;
+		margin-right: 8px;
+	}
+
+	nav {
+		padding: 1em 2em;
 		height: 72px;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		background-color: var(--bg-main-color);
 		flex-direction: row;
-		box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.3);
-		padding: 1em;
-		box-sizing: border-box;
+		box-shadow: 0 0px 8px rgba(0, 0, 0, 0.1);
+		border-top: 1px solid var(--nav-item-disabled);
+		border-bottom: 2px solid var(--nav-item-disabled);
 		color: var(--text-primary-color);
-		font-weight: bold;
-		font-size: 18px;
+		font-weight: 700;
+		font-size: 20px;
 	}
 
 	nav li {
@@ -75,9 +107,9 @@
 	}
 
 	nav a {
-		font-weight: normal;
+		font-weight: 600;
 		font-size: 16px;
 		text-decoration: none;
-		padding: 0 0.5em 0 0.5em;
+		padding: 0 0.4em;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,40 +3,102 @@
 	import rover2 from '$lib/assets/rover2.jpg?webp&h=200&w=240&fit=cover&metadata';
 	import rover3 from '$lib/assets/rover3.jpg?webp&h=200&w=240&fit=cover&metadata';
 	import rover4 from '$lib/assets/rover4.jpg?webp&h=200&w=240&fit=cover&metadata';
+	import { text } from 'svelte/internal';
 
-	let imgs = [rover1, rover2, rover3, rover4];
+	type Content = {
+		id: number;
+		img: any;
+		heading: string;
+		text: string;
+		alt: string;
+	};
+
+	let contents: Content[] = [
+		{
+			id: 1,
+			img: rover1,
+			heading: 'Feature 1',
+			alt: 'Feature 1',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed risus pretium. Enim sed faucibus turpis in eu mi bibendum neque egestas. Nascetur ridiculus mus mauris vitae ultricies leo. Quam vulputate dignissim suspendisse in est ante. Volutpat odio facilisis mauris sit amet massa vitae tortor condimentum. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Urna cursus eget nunc scelerisque viverra mauris. Eget arcu dictum varius duis at consectetur lorem donec massa. Eu facilisis sed odio morbi quis commodo odio aenean. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat.'
+		},
+		{
+			id: 2,
+			img: rover2,
+			heading: 'Feature 2',
+			alt: 'Feature 2',
+			text: 'Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Sodales neque sodales ut etiam sit amet. Dui nunc mattis enim ut tellus elementum. At tellus at urna condimentum mattis pellentesque. Ante in nibh mauris cursus. Ligula ullamcorper malesuada proin libero nunc consequat. Lobortis mattis aliquam faucibus purus in massa tempor nec feugiat. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Amet justo donec enim diam. Vel eros donec ac odio tempor orci. Vitae tempus quam pellentesque nec nam.'
+		},
+		{
+			id: 3,
+			img: rover3,
+			heading: 'Feature 3',
+			alt: 'Feature 3',
+			text: 'Sed lectus vestibulum mattis ullamcorper velit. A diam sollicitudin tempor id eu nisl nunc mi. Lectus urna duis convallis convallis tellus id interdum velit. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Turpis egestas sed tempus urna et pharetra. Sed risus ultricies tristique nulla aliquet. Mi eget mauris pharetra et ultrices neque ornare aenean. Turpis egestas sed tempus urna et pharetra pharetra massa. Vulputate enim nulla aliquet porttitor lacus luctus accumsan. Feugiat scelerisque varius morbi enim. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Purus in mollis nunc sed id semper risus. Venenatis cras sed felis eget velit aliquet sagittis id consectetur.'
+		},
+		{
+			id: 4,
+			img: rover4,
+			heading: 'Feature 4',
+			alt: 'Feature 4',
+			text: 'Sed enim ut sem viverra aliquet eget sit amet. Odio euismod lacinia at quis risus sed vulputate. At lectus urna duis convallis convallis tellus id interdum. Condimentum vitae sapien pellentesque habitant morbi. Leo vel orci porta non pulvinar neque laoreet suspendisse interdum. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Ultricies integer quis auctor elit sed. Vivamus at augue eget arcu dictum varius. Nisi est sit amet facilisis magna etiam tempor. Ante in nibh mauris cursus. Commodo viverra maecenas accumsan lacus vel facilisis.'
+		}
+	];
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
+<div class="hero">
+	<p>The MIT Rover is here!!!!!</p>
+	<p>
+		MIT Rover is an Unmanned Ground Vehicle capable of remote operation and environment monitoring.
+	</p>
+</div>
+<h1>Features</h1>
 <div class="content">
-	<h1>Welcome to MIT UGV Control Panel</h1>
-	<div class="images">
-		{#each imgs as img, i}
-			<img src={img.src} width={img.width} height={img.height} alt="rover picture {i + 1}" />
-		{/each}
-	</div>
-	<p>
-		Unmanned Ground Vehicles (UGVs) are evolving quickly in recent years for both military and
-		non-military uses, due to the rapid development of intelligence, sensor and vehicle control
-		technologies. A vehicle that works while in contact with the ground and without a human operator
-		is referred to as an unmanned ground vehicle (UGV). Any application where having a human
-		operator present could be difficult, risky, or impossible can employ UGVs.
-	</p>
-	<p>
-		Typically, a vehicle will include a collection of sensors to examine its surroundings and make
-		judgments about how to operate either on its own or by relaying information to a human operator
-		at a remote place who will teleoperate to manage the vehicle. Any UGV should ideally aim to
-		perceive its surroundings using sensors, localise itself inside it using software algorithms,
-		create a map of its surroundings if one does not already exist, plan its mobility and generate
-		the motor commands required to follow a certain course. The driving system utilised, the
-		machine's wheel or leg set and the feedback mechanism all have a big impact on these orders.
-	</p>
+	{#each contents as c (c.id)}
+		<div>
+			<img src={c.img.src} width={c.img.width} height={c.img.height} alt={c.alt} />
+			<div>
+				<h3>{c.heading}</h3>
+				<p>{c.text}</p>
+			</div>
+		</div>
+	{/each}
 </div>
 
 <style>
+	.hero {
+		width: 100%;
+		height: 40vh;
+		padding: 2em;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+		gap: 4px;
+		background: linear-gradient(15deg, hsl(46, 95%, 56%), hsl(350, 97%, 65%));
+		backdrop-filter: blur(6px);
+		color: hsl(0, 0%, 100%);
+		text-shadow: 0px 2px 10px hsla(0, 0%, 0%, 0.2);
+		font-size: 18px;
+		text-align: center;
+	}
+
+	h1 {
+		text-align: center;
+		padding-top: 1em;
+		text-decoration: dashed underline;
+		font-size: 36px;
+		font-weight: 600;
+	}
+
+	.hero :first-child {
+		font-size: 40px;
+		font-weight: 700;
+	}
+
 	.content {
 		padding: 4em;
 		display: flex;
@@ -44,25 +106,15 @@
 		flex-direction: column;
 	}
 
-	h1 {
-		text-align: center;
-	}
-
-	.images {
-		display: flex;
-		justify-content: space-between;
-		gap: 1em;
-		padding: 1em;
-	}
-
-	@media only screen and (max-width: 760px) {
-		.images {
-			flex-direction: column;
+	@media only screen and (min-width: 600px) {
+		.content > div {
+			display: flex;
+			width: 100%;
+			height: 30vh;
+			flex-direction: row;
+			justify-content: space-evenly;
+			align-items: center;
+			gap: 4em;
 		}
-	}
-
-	.images ~ p {
-		padding: 1em;
-		padding-bottom: 0em;
 	}
 </style>
